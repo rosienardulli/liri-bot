d1db462d85267f17f0cf68a55205fc15475b6dc9
Rosie Nardulli <rosie.nardulli@gmail.com>
2018-04-21 09:40:58 -0500

    liri bot

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: LRK2snfGuKKdydpJ7IiCa4jwS,
+  consumer_secret: deNBXhMV1G69Ve4CjwMBPfoVC54orKIS2L0haeiUKd97YXLX3h,
+  access_token_key: 987127802641121280-LdD9bOJTs6riV3YenBOSbvnl7Jg1IsR,
+  access_token_secret: mHqHEYXVEl4yxYMqg8pbDaEHMLEX1AIPep3AHtPwxhYJa
+};
+
+exports.spotify = {
+  id: 'acc8600390d242c790a951c7d182cac6',
+  secret: '178811a3eb9d41c6a658753205eacaa6',
+};
