989ee4272101475c5a5f714604db4bc6e98c3c0a
Rosie Nardulli <rosie.nardulli@gmail.com>
2018-04-23 13:19:54 -0500

    updated liri

--- liri.js
+++ liri.js
@@ -2,9 +2,9 @@ require("dotenv").config();
     // Node module imports needed to run the functions
 	var fs = require("fs"); //reads and writes files
 	var request = require("request");
-	var keys = require("./keys.js");
-	var twitter = require("twitter");
-	var spotify = require ("spotify");
+	var keys = require("keys.js");
+	var twitter = require("twitter");   //my-tweets
+	var spotify = require ("spotify");   //spotify-this-song
 	var liriArgument = process.argv[2];
 // ---------------------------------------------------------------------------------------------------------------
 	// Possible commands for this liri app
@@ -12,15 +12,7 @@ require("dotenv").config();
 		case "my-tweets": myTweets(); break;
 		case "spotify-this-song": spotifyThisSong(); break;
 		case "movie-this": movieThis(); break;
-		case "do-what-it-says": doWhatItSays(); break;
-		// Instructions displayed in terminal to the user
-		default: console.log("\r\n" +"Try typing one of the following commands after 'node liri.js' : " +"\r\n"+
-			"1. my-tweets 'any twitter name' " +"\r\n"+
-			"2. spotify-this-song 'any song name' "+"\r\n"+
-			"3. movie-this 'any movie name' "+"\r\n"+
-			"4. do-what-it-says."+"\r\n"+
-			"Be sure to put the movie or song name in quotation marks if it's more than one word.");
-	};
+    };
 // ---------------------------------------------------------------------------------------------------------------
 // Functions
 	// Movie function, uses the Request module to call the OMDB api
@@ -57,10 +49,10 @@ require("dotenv").config();
 	// Tweet function, uses the Twitter module to call the Twitter api
 	function myTweets() {
 		var client = new twitter({
-			consumer_key: keys.twitterKeys.consumer_key,
-			consumer_secret: keys.twitterKeys.consumer_secret,
-			access_token_key: keys.twitterKeys.access_token_key,
-			access_token_secret: keys.twitterKeys.access_token_secret, 
+			TWITTER_CONSUMER_KEY=your-twitter-consumer-key,
+            TWITTER_CONSUMER_SECRET=your-twitter-consumer-secret,
+            TWITTER_ACCESS_TOKEN_KEY=your-access-token-key,
+            TWITTER_ACCESS_TOKEN_SECRET=your-twitter-access-token-secret
 		});
 		var twitterUsername = process.argv[3];
 		if(!twitterUsername){
@@ -89,7 +81,7 @@ require("dotenv").config();
 	function spotifyThisSong(songName) {
 		var songName = process.argv[3];
 		if(!songName){
-			songName = "What's my age again";
+			songName = "I Want it That Way";
 		}
 		params = songName;
 		spotify.search({ type: "track", query: params }, function(err, data) {
@@ -113,22 +105,4 @@ require("dotenv").config();
 			}
 		});
 	};
-	// Do What It Says function, uses the reads and writes module to access the random.txt file and do what's written in it
-	function doWhatItSays() {
-		fs.readFile("random.txt", "utf8", function(error, data){
-			if (!error) {
-				doWhatItSaysResults = data.split(",");
-				spotifyThisSong(doWhatItSaysResults[0], doWhatItSaysResults[1]);
-			} else {
-				console.log("Error occurred" + error);
-			}
-		});
-	};
-	// Do What It Says function, uses the reads and writes module to access the log.txt file and write everything that returns in terminal in the log.txt file
-	function log(logResults) {
-	  fs.appendFile("log.txt", logResults, (error) => {
-	    if(error) {
-	      throw error;
-	    }
-	  });
-	}
+	
